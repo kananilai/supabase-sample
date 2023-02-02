1b72f553f6a372c88dcfacf86b15537e49cc4fba
kananilai <kana.nilai4646@gmail.com>
2023-02-02 15:48:41 +0900

    ⚡️

---
+++ src/Login.js
@@ -0,0 +1,68 @@
+// src/Login.jsx
+import { useState } from "react";
+import { supabase } from "./supabase";
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // ログイン・ユーザー登録を行う
+  const handleAuth = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (isLogin) {
+        const {
+          data: { user },
+          error,
+        } = await supabase.auth.signInWithPassword({ email, password });
+        if (error) throw error;
+      } else {
+        const { error } = await supabase.auth.signUp({
+          email: email,
+          password: password,
+        });
+        if (error) throw error;
+      }
+    } catch (error) {
+      alert(error.error_description || error.message);
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <div>
+        <h1> {`${isLogin ? "ログイン" : "登録"}`}</h1>
+      </div>
+      <div>
+        <form onSubmit={handleAuth}>
+          <div style={{ marginBottom: "16px" }}>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div style={{ marginBottom: "16px" }}>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div style={{ marginBottom: "16px" }}>
+            <span onClick={() => setIsLogin(!isLogin)}>
+              {`${isLogin ? "登録" : "ログイン"}モードへ切り替える`}
+            </span>
+          </div>
+          <div>
+            <button type="submit">{isLogin ? "ログイン" : "登録"}</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
